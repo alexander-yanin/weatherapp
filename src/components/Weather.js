5acf47249b3acf5685554a5d2b0a8ce1f337e3d5
Alexander Yanin <alexandr.nn54@gmail.com>
2017-06-12 01:07:51 +0700

    add weather info

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,11 +10,15 @@ function Weather({ city }) {
       </div>
       <p className="weather__cityName">{city.name} </p>
       <p className="weather__temp">{Math.floor(city.main.temp)}&deg;С</p>
-      <div className="weather__other_info">
+      <div className="weather__otherInfo clearfix">
         <div className="weather__otherInfoLeft">
+          <p> Мин. темп. {Math.floor(city.main.temp_min)}&deg;С</p>
           <p className="weather__otherInfo-wind">Ветер {city.wind.speed} м/с</p>
         </div>
-        <div className="weather__otherInfoRight"></div>
+        <div className="weather__otherInfoRight">
+          <p> Макс. темп. {Math.floor(city.main.temp_max)}&deg;С</p>
+          <p className=""> {city.weather[0].description} </p>
+        </div>
       </div>
     </div>
   );
